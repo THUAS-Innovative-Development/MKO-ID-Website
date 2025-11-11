d184a14de72640690ff67dee09a9b48ca237e627
JeroenRaaf <jeroenraaf@hotmail.com>
2025-11-11 13:14:51 +0100

    Added tailwind

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute('/main')({
 function Index() {
   return (
     <div className="p-2">
-      <h3>Welcome Home!</h3>
+      <h3 className='text-amber-700'>Welcome Home!</h3>
     </div>
   )
 }
